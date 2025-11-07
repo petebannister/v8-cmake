81c04b2440ed060c253373edf1efcaaad48deb5e
Pete Bannister <pete.bannister@gems.co.uk>
2025-11-07 14:14:51 +0000

    debugging build issue

--- cmake/GenerateBuiltinsList.cmake
+++ cmake/GenerateBuiltinsList.cmake
@@ -2,11 +2,13 @@ function(v8_generate_builtins_list target-dir)
   set(directory ${target-dir}/builtins-generated)
   set(output ${directory}/bytecodes-builtins-list.h)
   add_custom_command(
+    COMMAND echo make directory ${directory}...
     COMMAND ${CMAKE_COMMAND} -E make_directory ${directory}
     OUTPUT ${directory}
     COMMENT "Generating ${directory}"
     VERBATIM)
   add_custom_command(
+    COMMAND echo bytecode_builtins_list_generator ${output}...
     COMMAND bytecode_builtins_list_generator ${output}
     DEPENDS ${directory}
     OUTPUT ${output}
